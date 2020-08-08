b5d5c5e42f8833196b2f5608758bb554d2025409
Gustavo Martins <gugamm@live.com>
2020-08-08 00:47:31 -0300

    add @babel/plugin-proposal-optional-chaining as a dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,8 @@ module.exports = {
             ]
           ],
           plugins: [
-            ['@babel/plugin-proposal-class-properties', { loose: true }]
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            '@babel/plugin-proposal-optional-chaining'
           ]
         }
       }
